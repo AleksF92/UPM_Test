9d3a88e82ad80ad9ef7570d877613a0c3e1dcd90
Aleksander <aleks_f92@hotmail.com>
2019-05-31 21:35:38 +0200

    Added test script.

---
+++ Scripts/StringAsset.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Ledii/Variables/String Asset")]
+public class StringAsset : ScriptableObject
+{
+    [SerializeField]
+    [TextArea(3, int.MaxValue)]
+    private string value;
+
+    public string Value
+    {
+        get { return value; }
+    }
+}
